bb87cd3893942a79fb9c6756046ed969b4aee312
piyush-j0shi <thefarmguy011@gmail.com>
2025-08-12 09:55:59 +0000

    .rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
+enum Unit {
+    Gram,
+    Kilogram,
+    Milimiter,
+    Liter,
+    Cup,
+    spoon,
+}
+
+#[derive(Debug)]
+enum DietryTag {
+    Vegetarian,
+    NonVegetarian,
+}
+
+#[derive(Debug)]
+struct Ingredient {
+    name: String,
+    quantity: f64,
+    unit: Unit,
+    dietry_tag: DietryTag,
+}
+
+#[derive(Debug)]
+struct Recipe {
+    name: String,
+    ingredient: Vec<Ingredient>,
+    instructions: Vec<String>,
+    servings: u32,
+    tags: Vec<DietryTag>,
+}
+
+#[derive(Debug)]
+struct Substitution {
+    original: String,
+    alternatives: Vec<Ingredient>,
+}
+
+#[derive(Debug)]
+struct RecipeBook {
+    recipes: Vec<Recipe>,
+    substitutions: Vec<Substitution>,
+}
+
+#[derive(Debug)]
+struct MealPlan {
+    days: HashMap<String, Vec<Recipe>>,
+}
+
 fn main() {
     println!(".rs");
 }
