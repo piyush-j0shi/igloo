3e25395b1324bd4da87ac9d722b7fcfa29f8d14e
piyush-j0shi <thefarmguy011@gmail.com>
2025-08-19 17:28:43 +0000

    .rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,130 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
+enum PacketType {
+    TCP { scr_port: u16, dst_port: u16 },
+    UDP { scr_port: u16, dst_port: u16 },
+    Unknown,
+}
+
+#[derive(Debug)]
+struct Packet {
+    src_ip: String,
+    dst_ip: String,
+    packet_type: PacketType,
+    payload: Vec<u8>,
+}
+
+impl Packet {
+    fn summary(&self) {
+        println!("source ip : {}", self.src_ip);
+        println!("destination ip : {}", self.dst_ip);
+        println!("packet type : {:?}", self.packet_type);
+        println!("payload : {:#?}", self.payload);
+    }
+
+    fn size(&self) {
+        println!("size of payload : {}", self.payload.len());
+    }
+
+    fn is_suspicious(&self) -> bool {
+        if self.payload.is_empty() {
+            return true;
+        } else if self.payload.len() > 10000 {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Connection {
+    src_ip: String,
+    dst_ip: String,
+    src_port: u16,
+    dst_port: u16,
+    protocol: String,
+    packet_count: u64,
+    bytes_transferred: u64,
+}
+
+impl Connection {
+    fn new(packet: &Packet) -> Self {
+        let protocol_1 = match packet.packet_type {
+            PacketType::TCP { scr_port, dst_port } => "TCP".to_string(),
+            PacketType::UDP { scr_port, dst_port } => "UDP".to_string(),
+            PacketType::Unknown => "Unknown".to_string(),
+        };
+
+        Connection {
+            src_ip: packet.src_ip.clone(),
+            dst_ip: packet.dst_ip.clone(),
+            src_port: 80,
+            dst_port: 8080,
+            protocol: protocol_1,
+            packet_count: 0,
+            bytes_transferred: 0,
+        }
+    }
+
+    fn update(&mut self) -> Self {
+        self.packet_count += 1;
+        return self;
+    }
+}
+
+#[derive(Debug)]
+struct AnalyzeState {
+    connections: HashMap<String, Connection>,
+    events: Vec<String>,
+}
+
+impl AnalyzeState {
+    fn add_or_update_connection(&mut self, packet: &Packet) {
+        let the_key = format!(
+            "{}{}-{:?}",
+            packet.src_ip, packet.dst_ip, packet.packet_type
+        );
+
+        if self.connections.contains_key(&the_key) {
+            self.connections[the_key] = Connection::update();
+        } else {
+            self.connections.insert(the_key, Connection::new(packet));
+            println!("updated connection is : {:#?}", self.connections);
+        }
+    }
+}
+
 fn main() {
-    println!(".rs");
+    let packet = Packet {
+        src_ip: "127.0.0.1".to_string(),
+        dst_ip: "0.0.0.0".to_string(),
+        packet_type: PacketType::TCP {
+            scr_port: 80,
+            dst_port: 8080,
+        },
+        payload: vec![10, 20, 20],
+    };
+
+    let connections = Connection {
+        src_ip: "127.0.0.1".to_string(),
+        dst_ip: "192.168.0.10".to_string(),
+        src_port: 50000,
+        dst_port: 80,
+        protocol: "TCP".to_string(),
+        packet_count: 12,
+        bytes_transferred: 8421,
+    };
+
+    packet.summary();
+    let suspicion = packet.is_suspicious();
+    println!("Is Suspecied : {}", suspicion);
+
+    let mut analyzestate = AnalyzeState {
+        connections: HashMap::new(),
+        events: vec![],
+    };
+    analyzestate.add_or_update_connection(&packet);
+    analyzestate.add_or_update_connection(&packet);
 }
