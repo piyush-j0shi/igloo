b7700041c67272714b6894fcce2a1ec783d0096b
piyush-j0shi <thefarmguy011@gmail.com>
2025-08-25 07:06:38 +0000

    .rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
 fn main() {
-    let d = vec![1, 2, 3, 4, 5];
-    d[99];
+    let greeting_file_result = File::open("somefile.txt");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("somefile.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("problem creating file : {:?}", e),
+            },
+            _ => {
+                panic!("problem opening file : {:?}", error);
+            }
+        },
+    };
 }
