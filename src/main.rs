c2610fee88189cbf218e42e78679ef2d164f3699
piyush joshi <thefarmguy011@gmail.com>
2025-09-21 11:10:58 +0000

    third

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-fn printable<T, U>(t: T, u: U)
-where
-    T: std::fmt::Display,
-    U: std::fmt::Display,
-{
-    println!("first value is : {}", t);
-    println!("second value is : {}", u);
-}
-
 fn main() {
-    printable(42, "string");
+    println!("third");
 }
