93468e56cd965cd5e892ee33cb0f5785f8184245
piyush joshi <thefarmguy011@gmail.com>
2025-09-21 11:08:04 +0000

    second

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,12 @@
-trait Summarize {
-    fn summarize(&self) -> String;
-}
-
-#[derive(Debug)]
-struct Book {
-    author: String,
-    title: String,
-    publicationdate: String,
-}
-
-#[derive(Debug)]
-struct Movie {
-    name: String,
-    productionhouse: String,
-    releasedate: String,
-}
-
-impl Summarize for Book {
-    fn summarize(&self) -> String {
-        format!(
-            "{} by {} on {}",
-            self.title, self.author, self.publicationdate
-        )
-    }
-}
-
-impl Summarize for Movie {
-    fn summarize(&self) -> String {
-        format!(
-            "{} by {} on {}",
-            self.name, self.productionhouse, self.releasedate
-        )
-    }
-}
-
-fn print_summary<T: Summarize>(value: T) -> String {
-    value.summarize()
+fn printable<T, U>(t: T, u: U)
+where
+    T: std::fmt::Display,
+    U: std::fmt::Display,
+{
+    println!("first value is : {}", t);
+    println!("second value is : {}", u);
 }
 
 fn main() {
-    let book = Book {
-        author: "bookauthor".to_string(),
-        title: "booktitle".to_string(),
-        publicationdate: "bookpublicationdate".to_string(),
-    };
-
-    let movie = Movie {
-        name: "moviename".to_string(),
-        productionhouse: "movieproductionhouse".to_string(),
-        releasedate: "moviereleaseddate".to_string(),
-    };
-
-    println!("{}", book.summarize());
-    println!("{}", movie.summarize());
-
-    let book_details = print_summary(book);
-    println!("{}", book_details);
+    printable(42, "string");
 }
