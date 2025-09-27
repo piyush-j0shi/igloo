0841254293cfd57db4d319207011ef6989daf2b2
piyush joshi <thefarmguy011@gmail.com>
2025-09-27 19:32:58 +0000

    fourth

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,74 @@
-use std::fmt::Display;
+use std::f64::consts::PI;
 
-fn longest_with_announcement<'a, T: Display>(str1: &'a str, str2: &'a str, ann: T) -> &'a str {
-    println!("ann is : {ann}");
-    if str1.len() > str2.len() {
-        str1
-    } else {
-        str2
+trait Draw {
+    fn draw(&self) -> String;
+}
+
+trait Area {
+    fn area(&self) -> f64;
+}
+
+#[derive(Debug)]
+struct Circle {
+    radius: f64,
+}
+
+#[derive(Debug)]
+struct Square {
+    parametre: f64,
+}
+
+impl Draw for Square {
+    fn draw(&self) -> String {
+        format!(
+            "{}",
+            r#"+----+
+|    |
++----+"#
+        )
     }
 }
 
+impl Draw for Circle {
+    fn draw(&self) -> String {
+        format!(
+            "{}",
+            r#" /\
+/  \
+\  /
+ \/"#
+        )
+    }
+}
+
+impl Area for Circle {
+    fn area(&self) -> f64 {
+        PI * self.radius * self.radius
+    }
+}
+
+impl Area for Square {
+    fn area(&self) -> f64 {
+        self.parametre * self.parametre
+    }
+}
+
+fn lets_see<T>(value: T)
+where
+    T: Draw + Area,
+{
+    let ascii_drawing = value.draw();
+    let area_calculate = value.area();
+
+    println!("ascii drawing is ");
+    println!("{ascii_drawing}");
+    println!("area is : {area_calculate}");
+}
+
 fn main() {
-    let longest = longest_with_announcement(
-        "this is an announcement with longest",
-        "short announcement",
-        3,
-    );
-    println!("longest is : {longest}");
+    let circle = Circle { radius: 10.0 };
+    lets_see(circle);
+
+    let square = Square { parametre: 10.0 };
+    lets_see(square);
 }
