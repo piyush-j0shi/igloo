83cf8eb2694dceb33efe588360cb04304e7a061d
piyush joshi <thefarmguy011@gmail.com>
2025-09-27 18:53:01 +0000

    first

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+fn longest_string<'a>(str1: &'a str, str2: &'a str) -> &'a str {
+    if str1.len() > str2.len() {
+        str1
+    } else {
+        str2
+    }
+}
+
 fn main() {
-    println!("second");
+    let result = longest_string("thisislongeststring", "that");
+    println!("result : {result}");
 }
