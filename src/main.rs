0d8db3305fb1641dde76a02685b2a45122a2db5c
piyush joshi <thefarmguy011@gmail.com>
2025-09-29 06:34:29 +0000

    first

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("sixth");
+    let v1 = vec![1, 2, 3];
+
+    for val in v1.iter() {
+        println!("got : {val}");
+    }
+
+    let v1: Vec<i32> = vec![1, 2, 3];
+    let v2: Vec<_> = v1.iter().map(|x| x * 2).collect();
+
+    println!("v2 is : {:?}", v2);
 }
